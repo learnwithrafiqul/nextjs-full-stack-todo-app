ab57fd5aa2746dd6f4efede7b08c97fd919c31db
rafiqul-altersense <rafiqul.altersense@gmail.com>
2023-03-21 17:01:52 +0600

    get api created

--- pages/api/get-todos.js
+++ pages/api/get-todos.js
@@ -1,6 +1,11 @@
-export default function handler(req, res) {
+import clientPromise from "@/db/mongodb";
+
+export default async function handler(req, res) {
     if (req.method === 'GET') {
-        res.status(200).json({ name: 'John Doe' })
+        const client = await clientPromise
+        const db = await client.db("demo")
+        let todos = await db.collection('todo').find({}).toArray();
+        res.status(200).json(todos)
 
     } else {
         res.status(405).json({ message: 'Method not allowed' })
